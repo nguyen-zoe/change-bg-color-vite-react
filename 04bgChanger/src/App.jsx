9faf8c4f5f39157496c60d480c2d479c2faacb0d
Zoe Nguyen <znguyen2007@gmail.com>
2023-11-08 14:43:15 +0000

    change background color using vite-react

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -12,6 +12,7 @@ setColor(color);
 
   return (
     <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
+   
      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
       <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
          <button onClick={() =>changeColor('red')} className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor:'red'}}>Red</button>
